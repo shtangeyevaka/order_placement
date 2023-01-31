591e676f59d9d44a5ae5f0dd0def32a144267f0a
Shtangeeva Kseniya <zhiber.ksenya@gmail.com>
2023-01-31 08:18:59 +0500

    use fixtures in tests

--- tests/model/test_order.py
+++ tests/model/test_order.py
@@ -5,26 +5,29 @@ import pytest
 from order_placement.model import Order, HeadlightItem, EngineItem
 
 
-class TestOrder:
-    def setup(self):
-        self.order = Order()
+@pytest.fixture()
+def order_model():
+    return Order()
 
-    def test_add_item(self):
+
+def test_add_item(order_model):
+    with mock.patch.object(Order, 'items_changed') as items_changed_mock:
+        item = order_model.add_item(HeadlightItem.TITLE)
+
+    items_changed_mock.emit.assert_called_once_with([item], [])
+
+
+def test_remove_non_existing_item(order_model):
+    with pytest.raises(ValueError):
         with mock.patch.object(Order, 'items_changed') as items_changed_mock:
-            item = self.order.add_item(HeadlightItem.TITLE)
+            order_model.remove_item(HeadlightItem())
 
-        items_changed_mock.emit.assert_called_once_with([item], [])
+    items_changed_mock.emit.assert_not_called()
 
-    def test_remove_non_existing_item(self):
-        with pytest.raises(ValueError):
-            with mock.patch.object(Order, 'items_changed') as items_changed_mock:
-                self.order.remove_item(HeadlightItem())
 
-        items_changed_mock.emit.assert_not_called()
+def test_remove_existing_item(order_model):
+    item = order_model.add_item(EngineItem.TITLE)
+    with mock.patch.object(Order, 'items_changed') as items_changed_mock:
+        order_model.remove_item(item)
 
-    def test_remove_existing_item(self):
-        item = self.order.add_item(EngineItem.TITLE)
-        with mock.patch.object(Order, 'items_changed') as items_changed_mock:
-            self.order.remove_item(item)
-
-        items_changed_mock.emit.assert_called_once_with([], [item])
+    items_changed_mock.emit.assert_called_once_with([], [item])
